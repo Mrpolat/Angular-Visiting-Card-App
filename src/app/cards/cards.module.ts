9d97c75282dd504922e6109674d78e9c686d82c3
Mrpolat <m.polat.ucar@gmail.com>
2022-10-13 04:09:59 +0300

    cards component design : done

--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { CardsRoutingModule } from './cards-routing.module';
 import { CardsComponent } from './cards.component';
-
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,8 @@ import { CardsComponent } from './cards.component';
   ],
   imports: [
     CommonModule,
-    CardsRoutingModule
+    CardsRoutingModule,
+    MatCardModule
   ]
 })
 export class CardsModule { }
