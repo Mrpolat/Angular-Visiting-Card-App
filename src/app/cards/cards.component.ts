f87bcff7612e9faaf27140dcfd9e9d8413c9d0b7
Mrpolat <m.polat.ucar@gmail.com>
2022-10-19 02:37:06 +0300

    Modal Panel Form Design Done

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,3 +1,5 @@
+import { Card } from 'src/app/models/card';
+import { CardService } from './../services/card.service';
 import { CardModalComponent } from './card-modal/card-modal.component';
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
@@ -5,28 +7,24 @@ import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
-  styleUrls: ['./cards.component.scss']
+  styleUrls: ['./cards.component.scss'],
 })
 export class CardsComponent implements OnInit {
+  cards!: Card[];
 
-  cardItem = {
-    title:'Front-end',
-    name:'Muhsin Polat Uçar',
-    phone:'555 444 33 22',
-    email:'info@mpumpumpu.com',
-    address:'Ankara/Etimesgut'   
-  }
-
-  constructor(
-    public dialog : MatDialog
-  ) { }
+  constructor(public dialog: MatDialog, private cardService: CardService) {}
 
   ngOnInit(): void {
+    this.getCards();
   }
-  openAddCardModal():void{
-    this.dialog.open(CardModalComponent,{
-      width:'400px'
-    })
+  openAddCardModal(): void {
+    this.dialog.open(CardModalComponent, {
+      width: '400px',
+    });
   }
-
+  getCards(): void {
+    this.cardService.getCards().subscribe((res: Card[]) => {
+      this.cards = res;
+    });
+  }
 }
---
+++ src/app/services/card.service.ts
@@ -0,0 +1,19 @@
+import { Card } from 'src/app/models/card';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CardService {
+  
+  constructor(
+    @Inject('apiUrl') private apiUrl: string,
+    private http: HttpClient
+  ) { }
+
+  getCards(): Observable<Card[]>{
+    return this.http.get<Card[]>(this.apiUrl+"/cards");
+  }
+}
